0134e95a51b60cc6b3f5cb5bffca6e53ea89e5ea
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2022-11-09 00:17:32 +0100

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "components/Home/Home";
+
 export const App = () => {
   return (
-    <div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="movies" element={<Movies />}></Route>
+            <Route path="movies/:movieId" element={<MovieDetails />}></Route>
+        <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 };
+ 
